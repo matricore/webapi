993206ef7bdbdb06a964aa5dbb84b8b2d5606405
tezcan <tezcan@tpt.com.tr>
2017-05-11 12:48:27 +0300

    passport api authenticate + generate access token + login + throttle ok

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,27 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::post('api/login', 'API\UserController@login');
+Route::post('api/register', 'API\UserController@register');
+
+Route::group(['middleware' => ['auth:api','throttle:5,1']], function(){
+	Route::post('api/details', 'API\UserController@details');
+	Route::post('api/getall', 'API\UserController@getall');
+});
+
+
+// $app->group(['middleware' => 'throttle:60,1'], function () use ($app) {
+//     $app->get('/', function () use ($app) {
+//         return $app->version();
+//     });
+// });
+
+
+// Route::get('user/all', 'UserController@index');
+Route::post('api/user/create', 'UserController@createUser');
+
+
+// Route::get('api/user/all', 'UserController@getUsers');
+
+Route::get('api/user/all', ['middleware' => 'auth:api', 'before' => 'auth:api', 'uses' => 'UserController@getUsers']);
